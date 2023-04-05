0f1f4c033afceaa3d87e13476c8d286c16327497
Gustavo Choque <gustvjor2005@gmail.com>
2023-04-04 19:03:50 -0500

    test proces

--- routes/get.js
+++ routes/get.js
@@ -4,6 +4,8 @@ const app = express() //call express function
 const mysql = require('mysql');
 const config = require('config')
 
+//https://shapeshed.com/uncaught-exceptions-in-node/
+
 const conf = {
   host: 'localhost',
   user: 'root',
@@ -13,26 +15,28 @@ const conf = {
 
 const conn = mysql.createConnection(conf);
 
+var date = new Date()
 conn.connect((err) => {
     if (err){
-      console.log("Ocurrio error de conexion a la bd ", err);
+      console.log(date, "Ocurrio error de conexion a la bd ", err);
     } else {
-      console.log("Conectado a MYSQL Server");     
+      console.log(date, "Conectado a MYSQL Server");     
     }     
 });
 
 app.use(cors())
 app.listen(8091, ()=> {
-  console.log('APP escuchando en el puerto 8091')
+  var date = new Date()
+  console.log(date, 'APP escuchando en el puerto 8091')
 })
 
 app.get('/api/comment', (rq, rs)=>{
-  console.log("Express recibe GET."); 
+  var date = new Date()
+  console.log(date, "Express recibe GET."); 
   const sql_ = sqlGet(rq.get("ind"));
   conn.query(sql_, (err, rs_) => {
-
     if(err)
-      console.error('Error realizando consulta a bd')
+      console.error(date, 'Error realizando consulta a bd')
     else
       rs.json(rs_)
   })  
