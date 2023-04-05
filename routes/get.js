492ce799733c1832d4eb23cfd255f1a45eac8b59
Gustavo Choque <gustvjor2005@gmail.com>
2023-04-04 19:35:53 -0500

    update timezone

--- routes/get.js
+++ routes/get.js
@@ -2,9 +2,26 @@ const express = require('express')
 const cors = require('cors');
 const app = express() //call express function
 const mysql = require('mysql');
-const config = require('config')
+const config = require('config');
+const process = require('process');
 
 //https://shapeshed.com/uncaught-exceptions-in-node/
+
+//const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+var date = new Date().toLocaleString('es-PE', {
+    timeZone: 'America/Lima'
+});
+
+process.on('beforeExit', (code) => {
+  console.log(date, 'Process beforeExit event with code: ', code);
+});
+
+process.on('exit', (code) => {
+  console.log(date, 'Process exit event with code: ', code);
+});
+
+console.log(date, 'This message is displayed first.');
+
 
 const conf = {
   host: 'localhost',
@@ -15,10 +32,14 @@ const conf = {
 
 const conn = mysql.createConnection(conf);
 
-var date = new Date()
+var date = new Date().toLocaleString('es-PE', {
+    timeZone: 'America/Lima'
+});
+
 conn.connect((err) => {
     if (err){
       console.log(date, "Ocurrio error de conexion a la bd ", err);
+      return
     } else {
       console.log(date, "Conectado a MYSQL Server");     
     }     
@@ -26,19 +47,26 @@ conn.connect((err) => {
 
 app.use(cors())
 app.listen(8091, ()=> {
-  var date = new Date()
+  var date = new Date().toLocaleString('es-PE', {
+    timeZone: 'America/Lima'
+});
   console.log(date, 'APP escuchando en el puerto 8091')
 })
 
 app.get('/api/comment', (rq, rs)=>{
-  var date = new Date()
+  var date = new Date().toLocaleString('es-PE', {
+    timeZone: 'America/Lima'
+  });
   console.log(date, "Express recibe GET."); 
   const sql_ = sqlGet(rq.get("ind"));
   conn.query(sql_, (err, rs_) => {
-    if(err)
+    if(err){
       console.error(date, 'Error realizando consulta a bd')
-    else
+      return
+    }else{
       rs.json(rs_)
+    }
+
   })  
 })
 
