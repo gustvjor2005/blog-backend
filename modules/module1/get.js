7150475f26e28217fa64ce0e1d8cbe0f51636c46
gustavo.choque <gustvjor2005@gmail.com>
2024-05-20 18:02:08 -0500

    adding modules to the project

---
+++ modules/module1/get.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql");
+const config = require('config');
+
+const confDB = {
+  host : config.get('APP.host'),
+  user : "root",
+  password : config.get('APP.password'),
+  database : config.get('APP.database')
+};
+
+const pool = mysql.createPool(confDB);
+
+function sqlGet(ind){
+  return `SELECT * FROM comment WHERE section = "${ind}" ORDER BY day DESC `;
+}
+
+module.exports = function(app) {
+  app.get('/api/comment', (rq, rs)=>{
+    var date = new Date().toLocaleString('es-PE', {
+      timeZone: 'America/Lima'
+    });
+    console.log(date, "Router recibe GET."); 
+    const sql_ = sqlGet(rq.get("ind"));
+    pool.query(sql_, (err, rs_) => {
+      if(err){
+        console.error(date, 'Error realizando consulta a bd')
+        return
+      }else{
+        rs.json(rs_)
+      }
+    })  
+  })
+};
